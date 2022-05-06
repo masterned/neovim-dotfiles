038238dfa2c5d317f800f36ed511a2039470b51f
Spencer Dent <mr.spencerdent@gmail.com>
2022-05-06 18:08:07 -0400

    set many nvim settings w/ lua
    > following chris@machine's tutorial

---
+++ lua/user/options.lua
@@ -0,0 +1,45 @@
+local options =
+  { backup = false
+	, clipboard = "unnamedplus"
+	, cmdheight = 2
+	, completeopt = { "menuone", "noselect" }
+	, conceallevel = 0
+	, fileencoding = "utf-8"
+	, hlsearch = true
+	, ignorecase = true
+	, mouse = "a"
+	, pumheight = 10
+	, showmode = false
+	, showtabline = 2
+	, smartcase = true
+	, smartindent = true
+	, splitbelow = true
+	, splitright = true
+	, swapfile = false
+  --, termguicolors = true
+	, timeoutlen = 1000
+	, undofile = true
+	, updatetime = 300
+	, writebackup = false
+	, expandtab = true
+	, shiftwidth = 2
+	, tabstop = 2
+  --, cursorline = true
+	, number = true
+	, relativenumber = false
+	, numberwidth = 4
+	, signcolumn = "yes"
+	, wrap = false
+	, scrolloff = 8
+	, sidescrolloff = 8
+  --, guifont = "monospace:h17"
+  }
+
+for k, v in pairs(options) do
+  vim.opt[k] = v
+end
+
+vim.opt.shortmess:append "c"
+vim.opt.iskeyword:append "-"
+vim.opt.whichwrap:append "<,>,[,],h,l"
+
