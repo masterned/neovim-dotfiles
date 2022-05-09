6bd685285c3ea6acaa3ba8207ff79b2621919e79
Spencer Dent <mr.spencerdent@gmail.com>
2022-05-09 10:58:37 -0400

    introduce plug-ins
    > following chris@machine's tutorial
    
    > I commented-out the pop-up portion
    > as the color-scheme doesn't cooperate w/ it.
    > If I can fix the color-scheme,
    > I'll probably re-enable it.

---
+++ lua/user/plugins.lua
@@ -0,0 +1,51 @@
+local fn = vim.fn
+
+-- Automatically install packer
+local install_path = fn.stdpath "data" .. "/site/pack/packer/start/packer.nvim"
+if fn.empty(fn.glob(install_path)) > 0 then
+  PACKER_BOOTSTRAP = fn.system
+    { "git"
+    , "clone"
+    , "--depth"
+    , "1"
+    , "https://github.com/wbthomason/packer.nvim"
+    , install_path
+    }
+  print "Installing packer clone and reopen Neovim..."
+  vim.cmd [[packadd packer.nvim]]
+end
+
+-- Autocommand that reloads neovim whenever plugins.lua is saved
+vim.cmd [[
+  augroup packer_user_config
+    autocmd!
+    autocmd BufWritePost plugins.lua source <afile> | PackerSync
+  augroup end
+]]
+
+-- Protect call first use
+local status_ok, packer = pcall(require, "packer")
+if not status_ok then
+  return
+end
+
+-- Packer popup window
+--packer.init {
+  --display = {
+    --open_fn = function()
+      --return require("packer.util").float { border = "rounded" }
+    --end,
+  --},
+--}
+
+return packer.startup(function(use)
+  use "wbthomason/packer.nvim"  -- have packer manage itself
+  --use "nvim-lua/popup.nvim"   -- implement popup
+  use "nvim-lua/plenary.nvim"   -- lua helper functions
+
+  -- Sync & update all packages
+  if PACKER_BOOTSTRAP then
+    require("packer").sync()
+  end
+end)
+
