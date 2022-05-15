d8a232d69e5c9fae37e93fe0dee64621d333380a
Spencer Dent <mr.spencerdent@gmail.com>
2022-05-14 22:39:11 -0400

    install & config gitsigns
    > following chris@machine's tutorial

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -83,6 +83,9 @@ return packer.startup(function(use)
   use 'numToStr/Comment.nvim'
   use 'JoosepAlviste/nvim-ts-context-commentstring'
 
+  -- git
+  use 'lewis6991/gitsigns.nvim'
+
   -- Sync & update all packages
   if PACKER_BOOTSTRAP then
     require('packer').sync()
