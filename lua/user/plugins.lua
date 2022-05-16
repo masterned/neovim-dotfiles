542e1193fddce3926e77697a67d7afb7f815cce7
Spencer Dent <mr.spencerdent@gmail.com>
2022-05-16 11:56:02 -0400

    install & config bufferline
    > following chris@machine's tutorial
    
    > either this is not configured correctly,
    > there's another plugin I need,
    > or this plugin needs a little work...
    > it doesn't seem to play nice with others

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -90,6 +90,10 @@ return packer.startup(function(use)
   use 'kyazdani42/nvim-web-devicons'
   use 'kyazdani42/nvim-tree.lua'
 
+  -- bufferline
+  use 'akinsho/bufferline.nvim'
+  use 'moll/vim-bbye'
+
   -- Sync & update all packages
   if PACKER_BOOTSTRAP then
     require('packer').sync()
